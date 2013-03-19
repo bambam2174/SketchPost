a0e73787dea3a2c6bbc50784864a4fc8b0bde252
Sedat Kilinc <kilinc@adrodev.de>
2013-03-19 16:41:07 +0100

    update

---
+++ SketchPost/BamPickerController.h
@@ -0,0 +1,13 @@
+//
+//  BamPickerController.h
+//  SketchPost
+//
+//  Created by Sedat Kilinc on 19.03.13.
+//  Copyright (c) 2013 adrodev GmbH. All rights reserved.
+//
+
+#import <FacebookSDK/FacebookSDK.h>
+
+@interface BamPickerController : FBFriendPickerViewController
+
+@end
---
+++ SketchPost/BamPickerController.m
@@ -0,0 +1,38 @@
+//
+//  BamPickerController.m
+//  SketchPost
+//
+//  Created by Sedat Kilinc on 19.03.13.
+//  Copyright (c) 2013 adrodev GmbH. All rights reserved.
+//
+
+#import "BamPickerController.h"
+
+@interface BamPickerController ()
+
+@end
+
+@implementation BamPickerController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
